675837c27bf0edb23722e63f662beb845ac790b7
Oom <schooltechx@gmail.com>
2023-11-09 10:47:44 +0700

    add typedoc support

--- express-ts/src/lib/myapi.ts
+++ express-ts/src/lib/myapi.ts
@@ -1,6 +1,15 @@
 import express, { Express, Request, Response,NextFunction } from 'express'
 export const myapiRoute = express.Router()
 
+/** 
+ * ทักทายกลับไป
+ * @param name ชื่อที่รับเข้ามา
+ * @return Hello ตามด้วยชื่อที่รับเข้ามา
+*/
+export function hello(name:string){
+    return "Hello "+name
+}
+
 myapiRoute.get('/', (req, res) => {
     res.json({
         message: 'Hello from My API',
