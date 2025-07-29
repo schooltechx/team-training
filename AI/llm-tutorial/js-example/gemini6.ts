b698bdde7b39c52c7b85480fdda6e37d19a0101d
oom <sorawit@frappet.com>
2025-07-29 14:47:40 +0700

    improve gemini6.ts descripton

--- AI/llm-tutorial/js-example/gemini6.ts
+++ AI/llm-tutorial/js-example/gemini6.ts
@@ -1,9 +1,10 @@
 //external tools
-import { GoogleGenAI, Type,ContentListUnion } from '@google/genai';
+// Example call LLM twice if you really need external tools
+import { GoogleGenAI, Type,ContentListUnion,GenerateContentConfig } from '@google/genai';
 const model = "gemini-2.5-flash"
 let contents:ContentListUnion =[{
   role: 'user',
-  parts:[{text:"อุณหภูมิที่กรุงเทพตอนนี้เท่าไหร่?" }]
+  parts:[{text:"อุณหภูมิที่กรุงเทพตอนนี้เท่าไหร่?"}]
 }]
 // Define the function declaration for the model
 const weatherFunctionDeclaration = {
@@ -20,9 +21,8 @@ const weatherFunctionDeclaration = {
     required: ['location'],
   },
 };
-const config = {
-  tools: [{functionDeclarations: [weatherFunctionDeclaration]}],
-}
+const config:GenerateContentConfig = {
+  tools: [{functionDeclarations: [weatherFunctionDeclaration]}]}
 const getCurrentTemperature = (location:string)=>28
 const ai = new GoogleGenAI({});
 // Send request with function declarations
@@ -33,7 +33,6 @@ if (response.functionCalls && response.candidates &&
   response.candidates.length > 0 &&
   response.candidates[0].content &&
   response.functionCalls.length > 0) {
-
   const functionCall = response.functionCalls[0]; // Assuming one function call
   const t =  getCurrentTemperature(functionCall.args?.location as string);
   const function_response_part = {
@@ -44,6 +43,8 @@ if (response.functionCalls && response.candidates &&
   contents.push({ role: 'user', parts: [{ functionResponse: function_response_part }] });  
   const final_response = await ai.models.generateContent({model,contents,config })
   console.log("Final Response",final_response.text);
+  //{'role':'model',parts:[{functionCall:{...}}]}
+  // console.log(JSON.stringify(response.candidates[0].content,null,2))
 } else {
   console.log("Response:",response.text) //without function call
 }
